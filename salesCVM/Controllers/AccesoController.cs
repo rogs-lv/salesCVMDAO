d9e50a49cb31741a668e063245ddd4e85d9bb1a3
rogs <oliverrosasg@gmail.com>
2020-08-25 17:45:12 -0500

    Modifico proyecto a estructura incial

---
+++ salesCVM/Controllers/AccesoController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace salesCVM.Controllers
+{
+    public class AccesoController : ApiController
+    {
+    }
+}
