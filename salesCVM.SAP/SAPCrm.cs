e5e0f616f80e477d62daf0fafc307c908d4bf4d7
rogs <oliverrosasg@gmail.com>
2020-09-30 14:00:04 -0500

    Interface y clase para CRM SAP

---
+++ salesCVM.SAP/SAPCrm.cs
@@ -0,0 +1,32 @@
+﻿using salesCVM.SAP.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salesCVM.SAP
+{
+    public class SAPCrm : ISAPCrm
+    {
+        public bool CreateActivitie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateOportunity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateActivitie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateOportunity()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
